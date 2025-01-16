b9e5cbea15a1c4fc4564561a051a1d574a829061
ashishthoppil <ashishthoppil23@gmail.com>
2025-01-16 23:53:17 +0530

    JS injectors

--- public/js/loader.js
+++ public/js/loader.js
@@ -1,7 +1,7 @@
 (function() {
     setTimeout(() => {
       var chatbotScript = document.createElement('script');
-      chatbotScript.src = 'chatbot.js';
+      chatbotScript.src = 'https://mt-ai-chatbot.vercel.app/js/chatbot.js';
       chatbotScript.async = true;
       document.head.appendChild(chatbotScript);
     }, 4000)
