69f3bc97bdefa8e82c9e96f9e1ef66f11b6ce49f
ashishthoppil <ashishthoppil23@gmail.com>
2025-02-05 09:13:21 +0530

    loader script

--- public/js/loader.js
+++ public/js/loader.js
@@ -1,10 +1,10 @@
 (function() {
+    var loaderScriptURL = document.currentScript.src;
     var urlObject = new URL(loaderScriptURL);
     var id = urlObject.searchParams.get('id');
     localStorage.setItem('objectID', id);
     setTimeout(() => {
       var chatbotScript = document.createElement('script');
-
       chatbotScript.src = 'https://mt-ai-chatbot-git-main-ashishs-projects-33ba2137.vercel.app/js/chatbot.js?id=' + id;
       chatbotScript.async = true;
       document.head.appendChild(chatbotScript);
