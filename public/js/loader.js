9acdc93bcd7e1853b057289a9e04c2081eb53dd3
ashishthoppil <ashishthoppil23@gmail.com>
2025-02-05 09:08:28 +0530

    id added

--- public/js/loader.js
+++ public/js/loader.js
@@ -1,7 +1,10 @@
 (function() {
+    var id = urlObject.searchParams.get('id');
+    localStorage.setItem('objectID', id);
     setTimeout(() => {
       var chatbotScript = document.createElement('script');
-      chatbotScript.src = 'https://mt-ai-chatbot-git-main-ashishs-projects-33ba2137.vercel.app/js/chatbot.js';
+
+      chatbotScript.src = 'https://mt-ai-chatbot-git-main-ashishs-projects-33ba2137.vercel.app/js/chatbot.js?id=' + id;
       chatbotScript.async = true;
       document.head.appendChild(chatbotScript);
     }, 4000)
