a10a8262fe9ce3f432cfdbc35fcb4492fb2a41ef
ashishthoppil <ashishthoppil23@gmail.com>
2025-03-20 00:04:45 +0530

    Header issues

--- app/components/layout/Header.jsx
+++ app/components/layout/Header.jsx
@@ -94,7 +94,7 @@ export const Header = () => {
                     <button className='text-purple-800' onClick={() => setIsOpen((prev) => !prev)}>{isOpen ? <CloseTwoTone /> : <MenuIcon />}</button>}
                 </div>
                 <div className='hidden md:flex items-center gap-16'>
-                    {!ls && localStorage && !localStorage.getItem('objectID') ? <div className='flex items-center gap-5'>
+                    {ls && localStorage && !localStorage.getItem('objectID') ? <div className='flex items-center gap-5'>
                         <Link className='flex gap-1 text-purple-800 font-semibold' href=''><WorkflowIcon /> Use Cases</Link>
                         <button className='flex gap-1 text-purple-800 font-semibold'><ShapesIcon /> Book a Demo</button>
                         <Dialog onOpenChange={(open) => {
