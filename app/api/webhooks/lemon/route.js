e3731573605997c24d44dc7fa3252c63edaf3c89
ashishthoppil <ashishthoppil23@gmail.com>
2025-03-24 00:34:32 +0530

    Webhook test

---
+++ app/api/webhooks/lemon/route.js
@@ -0,0 +1,25 @@
+import clientPromise from "@/lib/mongodb";
+import { getDbName } from "@/lib/utils";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+    const payload = await req.json();
+
+    const DB_NAME = getDbName('Lumiscent India');
+    const client = await clientPromise;
+    const db = client.db(DB_NAME);
+
+//   if (payload.event_name === "subscription_created") {
+    const result = await db.collection('account').updateOne(
+        {},
+        {
+          $set: { 
+            isSubscribed: payload
+          },
+          $currentDate: { lastModified: true }
+        }
+    );
+//   }
+
+  return NextResponse.json({ success: true });
+}
